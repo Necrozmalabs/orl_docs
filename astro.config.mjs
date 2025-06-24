b270124fd3a1537d6e85c7de3d01e2adbdfecfe7
Abhinav Sharma <abhinavsharma@Abhinavs-MacBook-Air.local>
2025-06-24 16:20:00 +0530

    git

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://necrozmalabs.github.io',
-	base: '/orl_docs/',
+	base: '/orl_docs',
 	integrations: [
 		starlight({
 			title: 'Necrozma Open Research Laboratory',
