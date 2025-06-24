3bcc2f3516ceacbe2fd4f1dfbadf5bc08b7a8dc2
Abhinav Sharma <abhinavsharma@Abhinavs-MacBook-Air.local>
2025-06-24 18:10:00 +0530

    polish ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://necrozmalabs.github.io/orl_docs',
-	base: '/orl_docs',
+	site: 'https://documentation.necrozmalabs.com',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'Necrozma Open Research Laboratory',
 			social: [{ icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/company/necrozmalabs' }],
 			sidebar: [
-				
 				{
 					label: 'Guides',
 					autogenerate: { directory: 'guides' },
